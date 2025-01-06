b643de609c06df0f4e6c4ea7603527f7c72288c6
3 <masa.sverko@gmail.com>
2025-01-06 21:17:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,24 +3,30 @@ import google.generativeai as palm
 
 app = Flask(__name__)
 
-palm.configure(api_key='AIzaSyBIGETjjp18ap_9kc5_R4FI_O7eFQUkBlc')
+palm.configure(api_key='YOUR_API_KEY')
 
 def generate_response(query):
     try:
+        
         response = palm.chat(
             model="models/chat-bison-001", 
             messages=[{"role": "user", "content": query}],  
         )
         
-        print("API Response:", response)
+       
         if 'candidates' in response and len(response['candidates']) > 0:
             return response['candidates'][0]['content']
         else:
-            return "No valid response received."
-#    except Exception as e:
- #       print(f"Error generating response: {e}")
-  #      return "Došlo je do pogreške prilikom generiranja odgovora."
-
+            return "U moje vrijeme se nismo koristili ovakvim riječnikom, pokušaj ponovo."
+    except AttributeError as e:
+        print(f"AttributeError occurred: {e}")
+        return "Došlo je do pogreške prilikom dohvaćanja odgovora. Molimo pokušajte ponovo."
+    except KeyError as e:
+        print(f"KeyError occurred: {e}")
+        return "Došlo je do pogreške u procesu odgovora. Molimo pokušajte ponovo."
+    except Exception as e:
+        print(f"Error occurred: {e}")
+        return "Došlo je do pogreške prilikom generiranja odgovora."
 
 @app.route("/", methods=["GET", "POST"])
 def index():
