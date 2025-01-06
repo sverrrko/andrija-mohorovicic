ce8eb5ce7203706276de04ebdfae5cffa4e76bc7
3 <masa.sverko@gmail.com>
2025-01-06 20:34:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,35 @@
-
 from flask import Flask, render_template, request
 import google.generativeai as palm
 
 app = Flask(__name__)
 
-palm.configure(api_key='AIzaSyBIGETjjp18ap_9kc5_R4FI_O7eFQUkBlc')
+# Konfiguracija API ključa
+palm.configure(api_key='YOUR_API_KEY')
 
 def generate_response(query):
+    try:
 
-    response = palm.generate_text(
-        model='models/chat-bison-001',
-        prompt=f"""Ti si Andrija Mohorovičić, poznati hrvatski znanstvenik, odgovori na ovo pitanje kao da si on: {query}""",
-        temperature=0.7,
-        max_output_tokens=1024,
-    )
-    return response.result
+        response = palm.chat(
+            model="models/chat-bison-001", 
+            messages=[{"role": "user", "content": query}],  
+        )
+        return response['candidates'][0]['content'] 
+    except Exception as e:
+        print(f"Error generating response: {e}")
+        return "Došlo je do pogreške prilikom generiranja odgovora."
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     response = None
     if request.method == "POST":
-        query = request.form["query"]
-        response = generate_response(query)
+        query = request.form.get("query")  
+        if query:
+            response = generate_response(query) 
+        else:
+            response = "Nema upita."
     return render_template("index.html", response=response)
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 5000))  # Zadani port je 5000
+    port = int(os.environ.get("PORT", 5000))  
     app.run(host="0.0.0.0", port=port)
-
-
