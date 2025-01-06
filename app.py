e0817c644715b816edfad898ef5f44e666166b18
3 <masa.sverko@gmail.com>
2025-01-06 20:08:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -24,5 +24,9 @@ def index():
         response = generate_response(query)
     return render_template("index.html", response=response)
 
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Zadani port je 5000
+    app.run(host="0.0.0.0", port=port)
 
 
