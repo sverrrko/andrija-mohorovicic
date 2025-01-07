946af3d5d6eb89b6f43229454312198e5eb8bfc5
3 <masa.sverko@gmail.com>
2025-01-07 02:49:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,25 @@
 from flask import Flask, render_template, request
 import google.generativeai as palm
+import os
 
 app = Flask(__name__)
 
-palm.configure(api_key='AIzaSyBIGETjjp18ap_9kc5_R4FI_O7eFQUkBlc')  
+palm.configure(api_key=os.getenv('AIzaSyBIGETjjp18ap_9kc5_R4FI_O7eFQUkBlc'))
 
 def generiraj_odgovor(upit):
     """Generates a response from the Gemini API."""
     print(f"Generating response for query: {upit}")
-    odgovor = palm.generate_text(
-        model='models/chat-bison-001',
-        prompt=f"""Ti si Andrija Mohorovičić, hrvatski znanstvenik i seizmolog, odgovori na slijedeće pitanje kao da si on: {upit}""",
-        temperature=0.7,
-        max_output_tokens=1024,
-    )
-
-    print(f"Raw API response: {odgovor}")
+    try:
+        odgovor = palm.generate_text(
+            model='models/chat-bison-001',
+            prompt=f"""Ti si Andrija Mohorovičić, hrvatski znanstvenik i seizmolog, odgovori na slijedeće pitanje kao da si on: {upit}""",
+            temperature=0.7,
+            max_output_tokens=1024,
+        )
+        print(f"Raw API response: {odgovor}")
+    except Exception as e:
+        print(f"Error during API request: {e}")
+        return "Error during request"
 
     if odgovor and 'candidates' in odgovor and len(odgovor['candidates']) > 0:
         response_text = odgovor['candidates'][0]['output']
@@ -24,6 +28,7 @@ def generiraj_odgovor(upit):
     else:
         print("No valid response from the API.")
         return "No valid response from the API."
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     """Displays the chatbot interface and handles user queries."""
